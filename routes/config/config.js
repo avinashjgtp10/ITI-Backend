dfca260549e34f73351ad497c08447e603573d89
Avinash Balaso Jagtap <avinashjgtp10@gmail.com>
2019-06-05 22:53:32 +0530

    initial changes

--- routes/config/config.js
+++ routes/config/config.js
@@ -1,7 +1,16 @@
+// module.exports.databaseOptions = {
+//     host     : 'localhost',
+//     database : 'service_portal',
+//     user     : 'root',
+//     password : 'password',
+//     port     : '3306'
+//   };
+
 module.exports.databaseOptions = {
-    host     : 'localhost',
-    database : 'service_portal',
-    user     : 'root',
-    password : 'password',
-    port     : '3306'
-  };
+  host     : 'us-cdbr-iron-east-02.cleardb.net',
+  database : 'heroku_881a0b02b24c2eb',
+  user     : 'bf0655c159e23c',
+  password : '192c0a8c',
+  port     : '3306'
+};
+
