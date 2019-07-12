f02e3532ce96898a989d66305964c5f4c48f9d63
Avinash Balaso Jagtap <avinashjgtp10@gmail.com>
2019-07-12 23:11:04 +0530

    Server changes

--- app.js
+++ app.js
@@ -10,11 +10,11 @@ var config = require('./routes/config/config');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var complaintRouter = require('./routes/complaint');
-var cors=require('cors');
+var cors = require('cors');
 var app = express();
 
-app.use(bodyParser.json()); 
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }))
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -52,48 +52,58 @@ app.use('/users', usersRouter);
 app.use('/complaint', complaintRouter);
 app.get('/getData', function (req, res) {
     console.log("hi from server");
-    res.status(200).send({message: "Hi Raushan, How are You"});
+    res.status(200).send({ message: "Hi Raushan, How are You" });
 });
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+    next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
-var connection ;
+var connection;
 try {
-    function handleDisconnect() {
-        console.log("It always run")
-        connection = mysql.createConnection(config.databaseOptions); // Recreate the connection, since
-        connection.connect(function(err) {              // The server is either down
-            if(err) {                                     // or restarting (takes a while sometimes).
-                console.log('error when connecting to db:', err);
-                setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
-            }                                     // to avoid a hot loop, and to allow our node script to
-        });                                     // process asynchronous requests in the meantime.
-        connection.on('error', function(err) {
-            console.log('db error', err);
-            if(err.code === 'PROTOCOL_CONNECTION_LOST') {
-                handleDisconnect();
-            } else {
-                throw err;
-                handleDisconnect();
-            }
-        });
-    }
+    //     function handleDisconnect() {
+    //         connection = mysql.createConnection(config.databaseOptions);
+    //         // Recreate the connection, since
+    //         connection.connect(function (err) {              // The server is either down
+    //             if (err) {                                     // or restarting (takes a while sometimes).
+    //                 console.log('error when connecting to db:', err);
+    //                 setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+    //             }                                     // to avoid a hot loop, and to allow our node script to
+    //         });                                     // process asynchronous requests in the meantime.
+    //         connection.on('error', function (err) {
+    //             console.log('db error', err);
+    //             if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+    //                 handleDisconnect();
+    //             } else {
+    //                 throw err;
+    //                 handleDisconnect();
+    //             }
+    //         });
+    //     }
 
-    handleDisconnect();
+    //     handleDisconnect();
+
+    const pool = mysql.createPool(config.databaseOptions);
+
+    setInterval(() => {
+        pool.query('SELECT 1', (err, rows) => {
+            console.log("My error", err)
+            if (err) throw err;
+        });
+    }, 1000);
+
 }
-catch (err){
+catch (err) {
     console.log("error in sql connection");
 }
 
