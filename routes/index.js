6b8a573385cca3ecfd0867215cf1e0f72a761bec
Avinash Balaso Jagtap <avinashjgtp10@gmail.com>
2019-07-22 00:51:57 +0530

    initial level

--- routes/index.js
+++ routes/index.js
@@ -3,14 +3,14 @@ var router = express.Router();
 var mysql = require('mysql');
 
 var config=require('./config/config');
-var con= mysql.createConnection(config.databaseOptions);
-
+// var con= mysql.createConnection(config.databaseOptions);
+var con= config.databaseOptions;
 /* GET home page. */
 router.get('/', function(req, res, next) {
 con.connect((err)=>{
   if(err) throw err;
   console.log("Connected");
 })
-  res.render('index', { title: 'Express1' });
+  res.render('index', { title: 'new Express1' });
 });
 module.exports = router;
