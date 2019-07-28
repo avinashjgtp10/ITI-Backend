7011bea548b7fce9b1e3120659f753eb1c6431c0
Avinash Balaso Jagtap <avinashjgtp10@gmail.com>
2019-07-28 23:31:04 +0530

    Sunday 28/07/2019

--- routes/users.js
+++ routes/users.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 
 var config = require('./config/config');
 // var connection = mysql.createConnection(config.databaseOptions);
-let connection=config.databaseOptions;
+let connection = config.databaseOptions;
 
 router.get('/', function (req, res, next) {
   res.send('respond with a resource');
@@ -27,7 +27,7 @@ router.post('/login', cors(), function (req, res, next) {
       delete result[0].u_cpassword;
       res.send({ statusCode: res.statusCode, status: "success", data: result[0] });
     }
-  
+
   });
 });
 
@@ -61,26 +61,29 @@ router.get('/getStatus', cors(), function (req, res, next) {
 
 
 //// create New User  
-router.post('/createUser', function (req, res, next) {
-  console.log('connection is created for createUser');
-  var userObject = {
-    "u_name": req.body.u_name,
-    "u_mobile": req.body.u_mobile,
-    "u_altermobile": req.body.u_altermobile,
-    "u_email": req.body.u_email,
-    "u_address": req.body.u_address,
-    "u_MachinePurchased": req.body.u_MachinePurchased,
-    "u_dateOf_Purchased": req.body.u_dateOf_Purchased,
-    "u_password": req.body.u_password,
-    "u_cpassword": req.body.u_cpassword,
-    "u_role": req.body.u_role,
-    "u_roleType": req.body.u_roleType,
-    "u_joinDate": req.body.u_joinDate
-  }
-  connection.query('INSERT INTO user SET ?', userObject, function (err, result, fields) {
-    if (err) throw err;
-    result.message = "success";
-    res.send(result)
+router.post('/createUser', cors(), function (req, res, next) {
+
+  console.log('connection is created for createUser',JSON.stringify(req.body.u_name));
+  
+
+  var userObject = [[req.body.u_name], [req.body.u_mobile], [req.body.u_altermobile], [req.body.u_email], [req.body.u_address], [req.body.u_MachinePurchased], [req.body.u_dateOf_Purchased], [req.body.u_password], [req.body.u_cpassword],
+  [req.body.u_role],
+  [req.body.u_roleType],
+  [req.body.u_joinDate]
+  ]
+  // res.send({ name:userObject });  
+  console.log(userObject);
+
+  let sql = `INSERT INTO user (u_name, u_mobile,u_altermobile,u_email,u_address,u_MachinePurchased,u_dateOf_Purchased,u_password,u_cpassword,u_role,u_roleType,u_joinDate) VALUES ?`;
+  connection.query(sql, [userObject], function (err, result, fields) {
+    if (!result || err) {
+      console.log(err);
+      console.log(result)
+      res.send({ statusCode: res.statusCode, status: "error"+err });
+    } else {
+      console.log(result)
+      res.send({ statusCode: res.statusCode, status: "success" });
+    }
 
   });
 
